ba8eb83020c61b7ba2d089eca65b569a2a004e5f
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-15 17:35:20 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -11,5 +11,6 @@ def batch_badge_creator(array)
 end
 
 def assign_rooms(array)
+  lineSpot = array.index + 1
   array.collect {|x| "Hello,  #{x}! You'll be assigned to room #{array.index + 1}!"}
 end
