e5c8134d9d56de1e2b7437473c532839e163ba34
Bryan Ortiz <bryan@bryanortiz.com>
2017-12-15 06:01:03 +0000

    automatically backed up by learn

--- conference_badges.rb
+++ conference_badges.rb
@@ -1,9 +1,11 @@
 # Write your code here.
+attendees = ["Edsger", "Ada", "Charles", "Alan", "Grace", "Linus", "Matz"]
 
 def badge_maker(name)
   return "Hello, my name is #{name}."
 end
 
-def batch_badge_creator
-  
+def batch_badge_creator(array)
+  badgeArr = []
+  badgeArr = array.each {|x| return x, " -- " }
 end
